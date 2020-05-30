aa2e5d0fd7fa62341d13074f67f829110e0e76ba
Alex <alex@grbach.com>
2020-05-29 23:04:33 -0400

    add aurelia scaffolding

---
+++ aurelia_project/tasks/process-css.ts
@@ -0,0 +1,20 @@
+import {build} from 'aurelia-cli';
+import * as gulp from 'gulp';
+import * as project from '../aurelia.json';
+import * as sass from 'gulp-dart-sass';
+import * as postcss from 'gulp-postcss';
+import * as autoprefixer from 'autoprefixer';
+import * as cssnano from 'cssnano';
+import * as postcssUrl from 'postcss-url';
+
+export default function processCSS() {
+  return gulp.src(project.cssProcessor.source, {sourcemaps: true})
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer(),
+      postcssUrl({url: 'inline', encodeType: 'base64'}),
+      cssnano()
+    ]))
+    .pipe(build.bundle());
+}
+
